38f48a966871f03fb9f08ead87f1483f25c6c88a
AjayGupta <ajaygupta912098@gmail.com>
2024-03-14 10:51:50 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{
   console.log('mongodb connect');
