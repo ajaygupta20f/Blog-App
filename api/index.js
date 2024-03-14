ef49daa39c1b34ce9cae5419515843f049cb5fb7
AjayGupta <ajaygupta912098@gmail.com>
2024-03-14 11:59:31 +0530

    create  signup api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-// import authRoutes from './routes/auth.route.js';
+import authRoutes from './routes/auth.route.js';
 // import postRoutes from './routes/post.route.js';
 // import commentRoutes from './routes/comment.route.js';
 // import cookieParser from 'cookie-parser';
@@ -24,7 +24,7 @@ mongoose
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 // app.use(cookieParser());
 
 app.listen(3000, () => {
@@ -32,7 +32,7 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRoutes);
-// app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 // app.use('/api/post', postRoutes);
 // app.use('/api/comment', commentRoutes);
 
